3907268bdd5e43237f1299b746d98f0540adec6f
lxdnz254 <lxdnz34@gmail.com>
2016-05-18 12:05:01 +1200

    Movie Provider comlete, moved FetchMovieData to seperate class file ready for testing.

--- app/src/main/java/com/lxdnz/nz/movieproject/MovieGridFragment.java
+++ app/src/main/java/com/lxdnz/nz/movieproject/MovieGridFragment.java
@@ -2,37 +2,22 @@ package com.lxdnz.nz.movieproject;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Arrays;
-
 
 public class MovieGridFragment extends Fragment {
 
     private static final String LOG_CAT = MovieGridFragment.class.getSimpleName();
-    private GridView gridView;
-    private String dimension;
-    private ImageAdapter imageAdapter;
+    public GridView gridView;
+    public String dimension;
+    public ImageAdapter imageAdapter;
 
 
     public MovieGridFragment() {
@@ -61,7 +46,7 @@ public class MovieGridFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
 
-                Movie movieClicked = imageAdapter.getItem(position);
+                Movie movieClicked = imageAdapter.getMovie(position);
                 Intent intent = new Intent(getActivity(), MovieDetailActivity.class)
                         .putExtra("clickedMovie", movieClicked);
                 startActivity(intent);
@@ -73,13 +58,14 @@ public class MovieGridFragment extends Fragment {
     }
 
     private void updateMovies() {
-        FetchMovieData movieTask = new FetchMovieData();
+        FetchMovieData movieTask = new FetchMovieData(this);
         // get SharedPreferences
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(
                 getActivity());
         String filter = sharedPreferences.getString(getString(R.string.preference_sorting_key),
                 getString(R.string.preference_sorting_default));
         movieTask.execute(filter);
+
     }
 
     @Override
@@ -92,183 +78,4 @@ public class MovieGridFragment extends Fragment {
         return Integer.parseInt(dimension.substring(1));
     }
 
-    private class FetchMovieData extends AsyncTask<String, Void, Movie []> {
-
-        private final String LOG_TAG = FetchMovieData.class.getSimpleName();
-        private static final String SCHEME = "http";
-        private static final String AUTHORITY = "api.themoviedb.org";
-        private static final String AUTHORITY_IMAGE = "image.tmdb.org";
-        private static final String VERSION = "3";
-        private static final String MOVIE = "movie";
-        private static final String API_KEY = "api_key";
-        private static final String T = "t";
-        private static final String P = "p";
-
-        @Override
-        protected Movie [] doInBackground(String... param) {
-
-
-            // if there's no string then there's nothing to look up. Verify size of params
-            if (param.length == 0) {
-                return null;
-            }
-
-            // These three need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            String resultFromFetch;
-
-            try
-
-            {
-
-                // Construct the URL for the themoviedb.or query
-                // Possible parameters are avaiable at tmdb's API page, at
-                // http://www.themoviedb.org/documentation/API
-
-                Uri uri = new Uri.Builder().scheme(SCHEME)
-                        .authority(AUTHORITY)
-                        .appendPath(VERSION)
-                        .appendPath(MOVIE)
-                        .appendPath(param[0])
-                        .appendQueryParameter(API_KEY, BuildConfig.TMDB_API_KEY)
-                        .build();
-
-                Log.v(LOG_TAG, "uri = "+uri.toString());
-
-                URL url = new URL(uri.toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                resultFromFetch = buffer.toString();
-                try {
-                    return parseResponse(resultFromFetch);
-                } catch (JSONException e) {
-                    Log.e(LOG_TAG, e.getMessage(), e);
-                    e.printStackTrace();
-                }
-
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the movie data, there's no point in attempting
-                // to parse it.
-                return null;
-            }
-
-            finally
-
-            {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-
-            return null;
-        }
-
-        private Movie[] parseResponse(String resultFromFetch) throws JSONException {
-            final String RESULTS = "results";
-            final String POSTER_PATH = "poster_path";
-            final String OVERVIEW = "overview";
-            final String ID = "id";
-            final String ORIGINAL_TITLE = "original_title";
-            final String RELEASE_DATE = "release_date";
-            final String TITLE = "title";
-            final String BACKDROP_PATH = "backdrop_path";
-            final String VOTE_AVERAGE = "vote_average";
-            final String VOTE_COUNT = "vote_count";
-
-            JSONObject fetchResult = new JSONObject(resultFromFetch);
-            JSONArray results = fetchResult.getJSONArray(RESULTS);
-
-            Movie[] arrayOfMovies = new Movie[results.length()];
-
-            for (int i = 0; i < arrayOfMovies.length; ++i) {
-                JSONObject movieToParse = results.getJSONObject(i);
-
-
-                String overview = movieToParse.getString(OVERVIEW);
-                int id = movieToParse.getInt(ID);
-                String originalTitle = movieToParse.getString(ORIGINAL_TITLE);
-                String releaseDate = movieToParse.getString(RELEASE_DATE);
-                String title = movieToParse.getString(TITLE);
-
-                double voteAverage = movieToParse.getDouble(VOTE_AVERAGE);
-                int voteCount = movieToParse.getInt(VOTE_COUNT);
-
-                Uri uriBackdrop = new Uri.Builder().scheme(SCHEME)
-                        .authority(AUTHORITY_IMAGE)
-                        .appendPath(T)
-                        .appendPath(P)
-                        .appendPath(getString(R.string.preference_preview_width_default))
-                        .appendEncodedPath(movieToParse.getString(BACKDROP_PATH))
-                        .build();
-                String backdropPath = uriBackdrop.toString();
-
-                Uri uriPoster = new Uri.Builder().scheme(SCHEME)
-                        .authority(AUTHORITY_IMAGE)
-                        .appendPath(T)
-                        .appendPath(P)
-                        .appendPath(dimension)
-                        .appendEncodedPath(movieToParse.getString(POSTER_PATH))
-                        .build();
-                String posterPath = uriPoster.toString();
-                arrayOfMovies[i] = new Movie(title,
-                        originalTitle,
-                        releaseDate,
-                        overview,
-                        posterPath,
-                        backdropPath,
-                        id,
-                        voteCount,
-                        voteAverage);
-            }
-
-            return arrayOfMovies;
-        }
-
-        @Override
-        protected void onPostExecute(Movie [] result) {
-            super.onPostExecute(result);
-
-            if (result != null) {
-               imageAdapter = new ImageAdapter(getActivity(), Arrays.asList(result));
-                gridView.setAdapter(imageAdapter);
-            }
-        }
-    }
 }
--- app/src/main/java/com/lxdnz/nz/movieproject/data/MovieProvider.java
+++ app/src/main/java/com/lxdnz/nz/movieproject/data/MovieProvider.java
@@ -1,9 +1,11 @@
 package com.lxdnz.nz.movieproject.data;
 
+import android.annotation.TargetApi;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -59,22 +61,6 @@ public class MovieProvider extends ContentProvider{
         }
     }
 
-    @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
-    }
-
-    @Nullable
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
-    }
-
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
-    }
-
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
@@ -123,5 +109,154 @@ public class MovieProvider extends ContentProvider{
         }
         retCursor.setNotificationUri(getContext().getContentResolver(), uri);
         return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case MOVIE: {
+                long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = MovieContract.MovieEntry.buildMovieUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            case TRAILER: {
+                long _id = db.insert(MovieContract.TrailerEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = MovieContract.TrailerEntry.buildTrailersUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            case REVIEW: {
+                long _id = db.insert(MovieContract.ReviewEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = MovieContract.ReviewEntry.buildReviewUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into "+ uri);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+
+        if (null == selection) selection = "1";
+        switch (match) {
+            case MOVIE: {
+                rowsDeleted = db.delete(MovieContract.MovieEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            }
+            case TRAILER: {
+                rowsDeleted = db.delete(MovieContract.TrailerEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            }
+            case REVIEW: {
+                rowsDeleted = db.delete(MovieContract.ReviewEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsDeleted != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // this is like the delete function except update.
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match){
+            case MOVIE: {
+                rowsUpdated = db.update(MovieContract.MovieEntry.TABLE_NAME,values, selection, selectionArgs);
+                break;
+            }
+            case TRAILER: {
+                rowsUpdated = db.update(MovieContract.TrailerEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            }
+            case REVIEW: {
+                rowsUpdated = db.update(MovieContract.ReviewEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case TRAILER: {
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value: values) {
+                        long _id = db.insert(MovieContract.TrailerEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            }
+            case REVIEW: {
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value: values) {
+                        long _id = db.insert(MovieContract.ReviewEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            }
+            default:
+                return super.bulkInsert(uri, values);
+        }
+
+    }
+
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        mOpenHelper.close();
+        super.shutdown();
     }
 }
